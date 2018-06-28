c5bf0ebb7e5cf95d9cac11490c4f83164d8821ef
Graham <392235259@qq.com>
2018-06-28 14:59:51 +0800

    备份

--- src/app/shareholder/shareholder.component.ts
+++ src/app/shareholder/shareholder.component.ts
@@ -14,7 +14,7 @@ export class ShareholderComponent implements OnInit {
    dataSource:Observable<any>;
    hoders:Array<any>=[];
 
-  constructor(private routeInfo: ActivatedRoute,private http:Http) { 
+  constructor(private routeInfo: ActivatedRoute,private http:Http) {
 	let id=this.routeInfo.snapshot.params["id"];
   	let id_=id.substring(25);
 	this.dataSource=this.http.get('http://115.159.39.220:3444/relations/1/'+id_+"/total").pipe(map((res)=>res.json()));
--- src/app/the-investment/the-investment.component.ts
+++ src/app/the-investment/the-investment.component.ts
@@ -1,8 +1,8 @@
-import { Component,Input, Output,EventEmitter,OnInit } from '@angular/core';
-import {ActivatedRoute,Router} from '@angular/router';
-import { Http,Headers} from '@angular/http';
-import { map } from 'rxjs/operators';
-import { Observable,of} from 'rxjs';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Http, Headers} from '@angular/http';
+import {map} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
 import * as _ from 'lodash';
 
 @Component({
@@ -11,33 +11,36 @@ import * as _ from 'lodash';
   styleUrls: ['./the-investment.component.css']
 })
 export class TheInvestmentComponent implements OnInit {
-   dataSource:Observable<any>;
-   investments:Array<any>=[];
+  dataSource: Observable<any>;
+  investments: Array<any> = [];
 
-  constructor(private routeInfo: ActivatedRoute,private http:Http) { 
-  	let id=this.routeInfo.snapshot.params["id"];
-  	let id_=id.substring(25);
-	this.dataSource=this.http.get('http://115.159.39.220:3444/relations/0/'+id_).pipe(map((res)=>res.json()));
-    }
+  constructor(private routeInfo: ActivatedRoute, private http: Http) {
+    let id = this.routeInfo.snapshot.params["id"];
+    let id_ = id.substring(25);
+    this.dataSource = this.http.get('http://115.159.39.220:3444/relations/0/' + id_).pipe(map((res) => res.json()));
+    console.error("this.dataSource");
+    console.log("http://115.159.39.220:3444/relations/0/" + id_);
+  }
 
   ngOnInit() {
-   this.dataSource.subscribe((data)=>this.investments=data);
+    this.dataSource.subscribe((data) => this.investments = data);
   }
-  holdersFilter(holders){
-  let hoder=_.filter(holders,function(o){
-    return _.startsWith(o.value,'1');}
+
+  holdersFilter(holders) {
+    let hoder = _.filter(holders, function (o) {
+        return _.startsWith(o.value, '1');
+      }
     );
-  return _.uniqBy(hoder,'id');
-  }
-  moneyMatch(str){
-
-    if(escape(str).indexOf("%u")<0&&escape(str).indexOf("-")<0){
-    return str+"(万人民币)";
-    }
-    else{
-    return str;
-    }
+    return _.uniqBy(hoder, 'id');
   }
 
+  moneyMatch(str) {
 
+    if (escape(str).indexOf("%u") < 0 && escape(str).indexOf("-") < 0) {
+      return str + "(万人民币)";
+    }
+    else {
+      return str;
+    }
+  }
 }
